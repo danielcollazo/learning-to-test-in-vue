854120a41b15a3d45d65acf5f3dd5c8f7d97f388
Daniel Collazo <collazo.daniel@icloud.com>
2023-04-26 17:45:52 -0700

    Configure apollo cache persist

--- src/apollo/client-config.ts
+++ src/apollo/client-config.ts
@@ -1,10 +1,17 @@
+import LocalForage from 'localforage'
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client/core'
+import { LocalForageWrapper, persistCache } from 'apollo3-cache-persist'
 
 const httpLink = createHttpLink({
   uri: 'https://beta.pokeapi.co/graphql/v1beta'
 })
 
 const cache = new InMemoryCache()
+
+await persistCache({
+  cache,
+  storage: new LocalForageWrapper(LocalForage)
+})
 
 const client = new ApolloClient({
   link: httpLink,
